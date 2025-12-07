6db5c9be03f458af7430c7b9f8d3f8a38db9b1c1
eunhye <dmsgp2627@naver.com>
2025-12-07 18:22:59 +0900

    feat:이력서 id를 가져와서 반영할 수 있게 수정

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { dummyResumeData } from "../assets/assets";
 
 const initialResumeData = {
   _id: "",
@@ -21,10 +22,16 @@ const ResumeBuilder = () => {
   const [resumeData, setResumeData] = useState(initialResumeData);
 
   const loadExistingResume = () => {
+    const resume = dummyResumeData.find(resume => resume._id === resumeId)
+   if(!resume) return
 
+   setResumeData({...resume})
+   document.title = resume.title
   }
 
-  useEffect(()=>{},[])
+  useEffect(()=>{
+    loadExistingResume()
+  },[])
 
   return <div></div>;
 };
