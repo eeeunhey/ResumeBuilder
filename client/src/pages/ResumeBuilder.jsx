b05f3f39e184b2051da25550b53a49eafaea479f
eunhye <dmsgp2627@naver.com>
2025-12-07 16:59:45 +0900

    git 템플릿 테스트

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { useState } from "react"
+import { useParams } from "react-router-dom"
 
 const ResumeBuilder = () => {
+
+  const {resumeId} = useParams();
+
+  const [resumeData, setResumeData] = useState({
+    _id: '',
+    title:'',
+    personal_info: {}, 
+    professional_summary: "",
+    experience: [],
+    education: [],
+    project: [],
+    skills:[],
+    template: "classic",
+    accent_color: "#3B83F6",
+    public: false,
+  })
+  
   return (
-    <div>ResumeBuilder</div>
+    <div>
+
+    </div>
   )
 }
 
