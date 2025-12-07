d8d6f02e4a0d605b5d510b44bdf40f319c3b0173
eunhye <dmsgp2627@naver.com>
2025-12-07 20:36:58 +0900

    진행율 반영하기

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -2,7 +2,17 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { dummyResumeData } from "../assets/assets";
 import { Link } from "react-router-dom";
-import { ArrowLeftIcon, Briefcase, FileText, GraduationCap, User, FolderIcon, Sparkles } from "lucide-react";
+import {
+  ArrowLeftIcon,
+  Briefcase,
+  FileText,
+  GraduationCap,
+  User,
+  FolderIcon,
+  Sparkles,
+  ChevronLeft,
+  ChevronRight,
+} from "lucide-react";
 
 const initialResumeData = {
   _id: "",
@@ -31,14 +41,19 @@ const ResumeBuilder = () => {
     document.title = resume.title;
   };
 
+  const [activeSectionIndex, setActiveSectionIndex] = useState(0);
+  const [removeBackground, setRemoveBackground] = useState(false);
+
   const sections = [
-    {id: "personal", name: "Persionla Info", icon: User},
-    {id: " summary", name: "Summary", icon: FileText},
-    {id: "exprience", name: "Exprience", icon: Briefcase},
-    {id: "education", name: "Education", icon: GraduationCap},
-    {id: "projects", name: "Projects", icon:FolderIcon},
-    {id: "skills", name:"Skills", icon: Sparkles},
-  ]
+    { id: "personal", name: "Persionla Info", icon: User },
+    { id: " summary", name: "Summary", icon: FileText },
+    { id: "exprience", name: "Exprience", icon: Briefcase },
+    { id: "education", name: "Education", icon: GraduationCap },
+    { id: "projects", name: "Projects", icon: FolderIcon },
+    { id: "skills", name: "Skills", icon: Sparkles },
+  ];
+
+  const activeSection = sections[activeSectionIndex];
 
   useEffect(() => {
     loadExistingResume();
@@ -56,9 +71,62 @@ const ResumeBuilder = () => {
       </div>
 
       <div className="max-w-7xl mx-auto px-4 pb-8">
-        <div>
+        <div className="max-w-7xl mx-auto px-4 pb-8">
           {/* 왼쪽 부분 - 폼부분 */}
-          <div></div>
+          <div className="grid lg:grid-cols-12 gap-8">
+            <div className="relative lg:col-span-5 rounded-lg overflow-hidden">
+              <div className="bg-white rounded-lg shadow-sm border border-gray-200 p-6 pt-1">
+                {/* 진행상황 반영하는 부분 */}
+                <hr className="absolute top-0 left-0 right-0 border-2 border-gray-200 " />
+                <hr
+                  className="absolute top-0 left-0 h-1 bg-gradient-to-r from-green-500 to-green-600 border-none transition-all duration-2000"
+                  style={{
+                    width: `${
+                      (activeSectionIndex * 100) / (sections.length - 1)
+                    }%`,
+                  }}
+                />
+
+                {/* Section Navigation */}
+
+                <div className="flex justify-between items-center mb-6 border-b border-gray-300 py-1">
+                  <div></div>
+
+                  <div className="flex items-center">
+                    {activeSectionIndex !== 0 && (
+                      <button
+                        onClick={() =>
+                          setActiveSectionIndex((prevIndex) =>
+                            Math.min(prevIndex - 1, 0)
+                          )
+                        }
+                        className="flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover-gray-50 transition-all"
+                        disabled={activeSectionIndex === 0}
+                      >
+                        <ChevronLeft className="size-4" />
+                        이전
+                      </button>
+                    )}
+                    <button
+                      onClick={() =>
+                        setActiveSectionIndex((prevIndex) =>
+                          Math.min(prevIndex + 1, sections.length -1))} className={`
+                            flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all 
+                            ${activeSectionIndex === sections.length -1 && 'opacity-50'}`}
+                      disabled={activeSectionIndex === sections.length -1}
+                    >
+
+                      다음
+                      <ChevronRight className="size-4" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+              {sections.map((menu) => {
+                return <p key={menu.id}>{menu.name}</p>;
+              })}
+            </div>
+          </div>
 
           {/* 오른쪽 부분 - 미리보기 부분 */}
           <div></div>
