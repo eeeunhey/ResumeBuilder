152ec5471a510169dbd436184fdb617b0c4a72e4
eunhye <dmsgp2627@naver.com>
2025-12-08 20:30:51 +0900

    Feat:색상 지정을 위한 버튼 추가

---
+++ client/src/components/ColorPicker.jsx
@@ -0,0 +1,52 @@
+import { Palette } from "lucide-react";
+import { useState } from "react";
+
+const ColorPicker = ({selectedColor}) => {
+  const colors = [
+    { name: "Blue", value: "#3B82F6" },
+    { name: "Indigo", value: "#6366F1" },
+    { name: "Purple", value: "#8B5CF6" },
+    { name: "Green", value: "#10B981" },
+    { name: "Red", value: "#EF4444" },
+    { name: "Orange", value: "#F97316" },
+    { name: "Teal", value: "#14B8A6" },
+    { name: "Pink", value: "#EC4899" },
+    { name: "Gray", value: "#6B7280" },
+    { name: "Black", value: "#1F2937" },
+  ];
+
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="relative">
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="flex items-center gap-1 text-sm text-purple-600 bg-gradient-to-br from-purple-50 to-purple-100 ring-purple-300 hover:ring transition-all  px-3 py-2 rounded-lg"
+      >
+        <Palette size={16} /> <span className="max-sm:hidden">메인 색상</span>
+      </button>
+      {isOpen && (
+        <div className="grid grid-cols-4 w-60 gap-2 absolute top-full left-0 right-0 p-3 mt-2 z-10 ">
+          {colors.map((color) => (
+            <div
+              key={color.value}
+              onClick={() => {
+                onChange(color.value);
+              }}
+            >
+                                  <div>{color.name}</div>
+              {/* {seletedColor === color.value && (
+                <div>
+
+                </div>
+              )} */}
+            </div>
+          ))}
+        </div>
+        
+      )}
+      
+    </div>
+  );
+};
+
+export default ColorPicker;
